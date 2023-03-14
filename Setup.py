4e49b1da2171f69af5336f7df9757556601b656b
Spr_Aachen <2835946988@qq.com>
2023-03-14 23:11:01 +0800

    Add file for setuptools to work

---
+++ Setup.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "packageName",
+    version = '1.0.0',      
+    description = '',
+    author = "Spr_Aachen",
+    author_email = '2835946988@qq.com',
+    url = '',
+    packages = find_packages(),
+)
