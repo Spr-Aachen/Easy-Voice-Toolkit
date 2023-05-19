480b697c92c3137d6bcdefa5e316e5b803091a73
Spr_Aachen <2835946988@qq.com>
2023-05-19 16:57:46 +0800

    Add support for english model training

--- Tool_VoiceTrainer/vits/text/cleaners.py
+++ Tool_VoiceTrainer/vits/text/cleaners.py
@@ -1,7 +1,6 @@
 '''
 Edited
 '''
-""" from https://github.com/keithito/tacotron """
 
 '''
 Cleaners are transformations that run over the input text at both training and eval time.
@@ -17,13 +16,74 @@ hyperparameter. Some cleaners are English-specific. You'll typically want to use
 
 import re
 
-from .mandarin import number_to_chinese, chinese_to_bopomofo, latin_to_bopomofo
+from .mandarin import number_to_chinese, chinese_to_bopomofo, latin_to_bopomofo, chinese_to_ipa#, chinese_to_ipa2
+from .english import english_to_ipa2#, english_to_lazy_ipa2
+#from .chinesedialect import chinesedialect_to_ipa
 
 
-def chinese_cleaners(text):
-    '''Pipeline for Chinese text'''
-    text = number_to_chinese(text)
-    text = chinese_to_bopomofo(text)
-    text = latin_to_bopomofo(text)
-    text = re.sub(r'([ˉˊˇˋ˙])$', r'\1。', text)
-    return text
+def mandarin_cleaners(text):
+   '''Pipeline for Chinese text'''
+   text = number_to_chinese(text)
+   text = chinese_to_bopomofo(text)
+   text = latin_to_bopomofo(text)
+   text = re.sub(
+      pattern = r'([ˉˊˇˋ˙])$',
+      repl = r'\1。',
+      string = text
+   )
+   return text
+
+
+def mandarin_english_cleaners(text):
+   text = re.sub(
+      pattern = r'\[ZH\](.*?)\[ZH\]',
+      repl = lambda x: chinese_to_ipa(x.group(1)) + ' ',
+      string = text
+   )
+   text = re.sub(
+      pattern = r'\[EN\](.*?)\[EN\]',
+      repl = lambda x: english_to_ipa2(x.group(1)) + ' ',
+      string = text
+   )
+   text = re.sub(
+      pattern = r'\s+$',
+      repl = '',
+      string = text
+   )
+   text = re.sub(
+      pattern = r'([^\.,!\?\-…~])$',
+      repl = r'\1.',
+      string = text
+   )
+   return text
+
+
+'''
+def mandarin_english_chinesedialect_cleaners(text):
+   text = re.sub(
+      pattern = r'\[ZH\](.*?)\[ZH\]',
+      repl = lambda x: chinese_to_ipa2(x.group(1)) + ' ',
+      string = text
+   )
+   text = re.sub(
+      pattern = r'\[EN\](.*?)\[EN\]',
+      repl = lambda x: english_to_lazy_ipa2(x.group(1)) + ' ',
+      string = text
+   )
+   text = re.sub(
+      pattern = r'\[([A-Z]{2})\](.*?)\[\1\]',
+      repl = lambda x: chinesedialect_to_ipa(x.group(2), x.group(1)).replace('ʣ', 'dz').replace('ʥ', 'dʑ').replace('ʦ', 'ts').replace('ʨ', 'tɕ') + ' ',
+      string = text
+   )
+   text = re.sub(
+      pattern = r'\s+$',
+      repl = '',
+      string = text
+   )
+   text = re.sub(
+      pattern = r'([^\.,!\?\-…~])$',
+      repl = r'\1.',
+      string = text
+   )
+   return text
+'''
