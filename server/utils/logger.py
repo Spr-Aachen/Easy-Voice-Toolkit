c9988ce8014c14da48291b07e612bb22f7ba0ff9
Foxie-404 <2038834945@qq.com>
2025-09-22 13:47:25 +0800

    Simplify logger creation - Move logger creation function to custom library - Refactor createLogger function to loggerManager class
    Co-authored-by: Spr_Aachen <2835946988@qq.com>

--- server/utils/logger.py
+++ server/utils/logger.py
@@ -1,47 +1,6 @@
 import sys
-from loguru import logger
 from pathlib import Path
-from typing import Optional
-
-#############################################################################################################
-
-_loggers_initialized = False
-
-
-def createLogger(
-    name: str,
-    level: str = "INFO",
-    format: str = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
-    outputPath: Optional[str] = None,
-    rotation: str = "10 MB",
-):
-    global _loggers_initialized
-
-    if not _loggers_initialized:
-        logger.remove()
-        _loggers_initialized = True
-
-    logger.add(
-        sys.stderr,
-        format = format,
-        level = level,
-    )
-
-    if outputPath:
-        dir = Path(outputPath).parent
-        dir.mkdir(parents = True) if not dir.exists() else None
-
-        logger.add(
-            Path(outputPath).as_posix(),
-            level = level,
-            format = format,
-            backtrace = True,
-            diagnose = True,
-            enqueue = True,
-            rotation = rotation,
-        )
-
-    return logger.bind(name = name)
+from PyEasyUtils import loggerManager
 
 #############################################################################################################
 
@@ -49,13 +8,15 @@ currentDir = Path(sys.argv[0]).parent.as_posix()
 
 logDir = Path(currentDir).joinpath('logs').as_posix()
 
-infoLogger = createLogger(
+logger = loggerManager()
+
+infoLogger = logger.createLogger(
     name = "router",
     level = "INFO",
     outputPath = Path(logDir).joinpath("server.log"),
 )
 
-errorLogger = createLogger(
+errorLogger = logger.createLogger(
     name = "error",
     level = "ERROR",
     outputPath = Path(logDir).joinpath("server.log"),
